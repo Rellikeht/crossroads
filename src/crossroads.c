46db70df997c374a38cd316efedface7a5ce4622
Rellikeht <michaelhemperek@gmail.com>
2025-02-09 12:52:32 +0100

    add queuing of cars

--- src/crossroads.c
+++ src/crossroads.c
@@ -1,7 +1,44 @@
 #include "crossroads.h"
 #include <stdio.h>
 
+void add(Road *road, bool left, Car car) {
+  Car *lane;
+  int *last;
+  if (left) {
+    lane = road->left_lane;
+    last = &road->left_last;
+  } else {
+    lane = road->main_lane;
+    last = &road->main_last;
+  }
+  lane[*last] = car;
+  *last = (*last + 1) % ROAD_LENGTH;
+}
+
+bool change() {
+  // TODO
+  return true;
+}
+
 void simulate() {
-  printf("Hello ");
-  printf("World!\n");
+  Road roads[4] = {0};
+  bool north_south = true, left_turn = false;
+  int wait_time = 0;
+
+  while (!feof(stdin)) {
+    int command = 0;
+
+    scanf("%i", &command);
+    if (command != 0) {
+      Car newCar = {0};
+      int position = 0;
+      scanf("%i %i %i", &newCar.id, &position, &newCar.destination);
+      add(&roads[position],
+          newCar.destination - position == LEFT_TURN,
+          newCar);
+      continue;
+    }
+
+    // TODO simulate !!
+  }
 }
--- src/crossroads.h
+++ src/crossroads.h
@@ -1,1 +1,17 @@
+#include <stdbool.h>
+#define ROAD_LENGTH 1024
+#define LEFT_TURN   -1
+
+typedef struct {
+  int id, destination;
+} Car;
+
+typedef struct {
+  Car main_lane[ROAD_LENGTH], left_lane[ROAD_LENGTH];
+  int main_first, left_first;
+  int main_last, left_last;
+} Road;
+
+void add(Road *road, bool left, Car car);
+
 void simulate();
